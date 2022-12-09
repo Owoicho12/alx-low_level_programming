93431132e2771cc53e1e336e8f8e1749155b09c1
Owoicho12 <owoichoamah@yahoo.com>
2022-12-09 11:49:13 -0800

    task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-/*
-* main - Entry prints all the numbers of base 16 in lowercase
-* Return: Always 0 (Success)
-*/
+
+/**
+ * main - prints all the numbers of base 16 in lowercase
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
 	int num = '0'; /*Initialize num to 0*/
@@ -21,6 +23,5 @@ int main(void)
 	}
 
 	putchar('\n');
-
 	return (0);
 }
