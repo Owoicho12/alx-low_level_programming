953271f33aa4518b13684efbb25e57acf3fcdaa2
Owoicho12 <owoichoamah@yahoo.com>
2022-12-13 11:49:05 -0800

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- *  * jack_bauer - prints every minute of the day
- *   *
- *    * Return: Always 0.
- *     */
+ * jack_bauer - prints every minute of the day
+ *
+ * Return: Always 0.
+ */
 
 void jack_bauer(void)
 {
