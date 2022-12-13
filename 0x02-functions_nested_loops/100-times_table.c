a7836345fe5df1be78045382286e3af83fdd117a
Owoicho12 <owoichoamah@yahoo.com>
2022-12-13 12:42:02 -0800

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,12 +1,10 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
  * print_times_table - prints time table with provided number
  * @n: the provided value by the user
  * Return: Always 0 (Success)
  */
-
 void print_times_table(int n)
 {
 	int row;
