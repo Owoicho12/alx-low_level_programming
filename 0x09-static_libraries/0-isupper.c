8a9e2b9ea77315a9c42381d7eb253b2f71324cd3
Owoicho12 <owoichoamah@yahoo.com>
2023-01-06 07:26:41 -0800

    task 1

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _isupper - uppercase letters
+ * @c: char to check
+ *
+ * Return: 0 or 1
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c < 91)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+
+}
